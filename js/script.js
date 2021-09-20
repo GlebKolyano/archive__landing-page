0f84aa165c4113463b88e45f76096ccbba41adeb
GlebKolyano <glebkolyano38@gmail.com>
2021-09-20 14:09:03 +0300

    made prev and next buttons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /******/ (function() { // webpackBootstrap
 var __webpack_exports__ = {};
 $(function () {
-  $('.slider__inner').slick();
+  $('.slider__inner').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    infinite: false
+  });
 });
 /******/ })()
 ;
